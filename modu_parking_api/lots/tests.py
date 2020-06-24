70e66be7545a9332e6e771b3ae38a8bd61ddf7c2
Jamie-Cheon <jamiecheon55@gmail.com>
2020-06-24 19:51:26 +0900

    test: add lotsapp test

--- modu_parking_api/lots/tests.py
+++ modu_parking_api/lots/tests.py
@@ -1,12 +1,70 @@
-# from django.db import models
-# from django.contrib.gis.db import models as db_models
-# from django.contrib.auth import get_user_model
-# from model_bakery import baker
-# from rest_framework import status
-# from rest_framework.authtoken.models import Token
-# from rest_framework.test import APITestCase, CoreAPIClient
-#
-# # User = get_user_model()
-# # from lots.models import Lot
+from rest_framework.test import APITestCase
+from lots.models import Lot
+from haversine import haversine
 
-"""Nothing"""
+
+class LotsTestCase(APITestCase):
+    def setUp(self) -> None:
+        self.lat = 37.5
+        self.lng = 126.5
+
+        # create lots with different location
+        for i in range(50):
+            Lot.objects.create(latitude=self.lat, longitude=self.lng, name=f'{i}lot', )
+            self.lat += 0.05
+            self.lng += 0.01
+        self.user_data = {
+            'latitude': self.lat,
+            'longitude': self.lng,
+        }
+
+    def test_map_list(self):
+        user_location = {
+            'latitude': self.lat,
+            'longitude': self.lng,
+            'zoom_lv': 2  # In order to retrieve lots within 2km
+        }
+        response = self.client.get('/api/lots/map', data=user_location)
+        self.assertEqual(response.status_code, 200)
+
+        user_location = (self.lat, self.lng)
+        for lot in response.data:
+            lat = lot['latitude']
+            lng = lot['longitude']
+            lot_location = (lat, lng)
+
+            distance = haversine(lot_location, user_location)
+            self.assertLessEqual(distance, 2)  # distance between user and lot should be within 2km
+
+    def test_distance_odr_list(self):
+        response = self.client.get('/api/lots/distance_odr', data=self.user_data)
+        self.assertEqual(response.status_code, 200)
+
+        # check if the response shows lots in distance order
+        user_location = (self.lat, self.lng)
+        for lot in response.data:
+            lat = lot['latitude']
+            lng = lot['longitude']
+            lot_location = (lat, lng)
+
+            distance = haversine(lot_location, user_location)
+
+            lot['distance'] = distance
+
+        # sorting lots with distance
+        sorted_lots = sorted(response.data, key=lambda x: x['distance'])
+        for res, lot in zip(response.data, sorted_lots):
+            self.assertEqual(res['id'], lot.id)
+            self.assertEqual(res['name'], lot.name)
+        # self.assertEqual(response.data, sorted_lots)
+
+    def test_price_odr_list(self):
+        response = self.client.get('/api/lots/price_odr', data=self.user_data)
+        self.assertEqual(response.status_code, 200)
+
+        # sorting lots with price
+        sorted_lots = sorted(response.data, key=lambda x: x['basic_rate'])
+
+        for res, lot in zip(response.data, sorted_lots):
+            self.assertEqual(res['id'], lot.id)
+            self.assertEqual(res['name'], lot.name)
--- modu_parking_api/lots/views.py
+++ modu_parking_api/lots/views.py
@@ -1,78 +1,15 @@
-from django.contrib.gis.geos import Point
-from django.contrib.gis.measure import D
-from django.shortcuts import render
 from rest_framework import viewsets
-from rest_framework.decorators import action
-from rest_framework.response import Response
-
-from lots.filters import OrderedDistanceToPointFilter
 from lots.models import Lot
-from lots.serializers import LotsSerializer
-from users.models import User
-import math
+from lots.serializers import LotsSerializer, OrderSerializer, MapSerializer
 
 
 class LotsViewSet(viewsets.ModelViewSet):
-    """
-    사용자에게 주차장들을 보여줌
-    1. order : 정렬해서 목록으로
-    2. map : 일정범위만큼 잘라서
-
-    order :
-
-
-    """
     queryset = Lot.objects.all()
     serializer_class = LotsSerializer
-    OrderedDistanceToPointFilter()
 
-    # def filter_queryset(self, queryset):
-    #     if self.request.data['order'] == 'price':
-    #         self.queryset = Lot.objects.filter().order_by('basic_rate')
-    #     elif self.request.data['order'] == 'distance':
-    #
-    #         distance = self.request.data.pop('zoom_level')  # 줌레벨
-    #         lat = self.request.data['lat']
-    #         lon = self.request.data['lon']
-    #
-    #         ref_location = Point(lon, lat)
-    #         queryset = Lot.objects.filter(
-    #             location__distance_lte=(
-    #                 ref_location,
-    #                 D(m=distance)
-    #             )
-    #         ).distance(ref_location).order_by('distance')
-    #         return queryset
-    #
-    #     return super().filter_queryset(queryset)
-
-    @action(detail=False)
-    def order(self, request, *args, **kwargs):
-        # page = self.paginate_queryset(queryset)
-        # if page is not None:        #     serializer = self.get_serializer(page, many=True)
-        #     return self.get_paginated_response(serializer.data)
-
-        serializer = self.get_serializer(self.queryset, many=True)
-        return Response(serializer.data)
-
-    @action(detail=False)
-    def order_distance(self, request, *args, **kwargs):
-        return super().list(request, *args, **kwargs)
-
-    def list(self, request, *args, **kwargs):
-        pass
-
-
-def distance(origin, destination):
-    lat1, lon1 = origin
-    lat2, lon2 = destination
-    radius = 6371  # km
-
-    dlat = math.radians(lat2 - lat1)
-    dlon = math.radians(lon2 - lon1)
-    a = math.sin(dlat / 2) * math.sin(dlat / 2) + math.cos(math.radians(lat1)) \
-        * math.cos(math.radians(lat2)) * math.sin(dlon / 2) * math.sin(dlon / 2)
-    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
-    d = radius * c
-
-    return d
+    def get_serializer(self, *args, **kwargs):
+        if self.action in ('distance_odr', 'price_odr'):
+            return OrderSerializer(*args, **kwargs)
+        elif self.action == 'map':
+            return MapSerializer(*args, **kwargs)
+        return super().get_serializer(*args, **kwargs)
