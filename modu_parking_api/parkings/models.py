06208d4ca7eec419b71b5f7d37bf16133054a315
Jamie-Cheon <jamiecheon55@gmail.com>
2020-06-24 15:39:00 +0900

    setting pull

--- modu_parking_api/parkings/models.py
+++ modu_parking_api/parkings/models.py
@@ -1,12 +1,14 @@
 from django.db import models
-
 from lots.models import Lot
 from users.models import User
-
+from lots.models import basicRate, additionalRate
 
 class Parking(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='parkings')
     lot = models.ForeignKey(Lot, on_delete=models.CASCADE)
+    basicRate = models.ForeignKey(basicRate, on_delete=models.CASCADE)
+    additionalRate = models.ForeignKey(additionalRate, on_delete=models.CASCADE)
     total_fee = models.IntegerField(default=0)
-    start_time = models.DateTimeField()
+    start_time = models.DateTimeField(auto_now_add=True)
     parking_time = models.DateTimeField()
+    current_stat = models.BooleanField()
