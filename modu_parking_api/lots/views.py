9bdc57f2077d2f2e5b54d7c298f278804f2e402f
Jamie-Cheon <jamiecheon55@gmail.com>
2020-06-24 21:14:48 +0900

    add filter

--- modu_parking_api/lots/views.py
+++ modu_parking_api/lots/views.py
@@ -1,4 +1,3 @@
-import math
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
@@ -11,12 +10,6 @@ from lots.serializers import LotsSerializer, OrderSerializer, MapSerializer
 
 
 class LotsViewSet(viewsets.ModelViewSet):
-    """
-    사용자에게 주차장들을 보여줌
-    1. order : 정렬해서 목록으로
-    2. map : 일정범위만큼 잘라서
-    order :
-    """
     queryset = Lot.objects.all()
     serializer_class = LotsSerializer
 
@@ -27,20 +20,34 @@ class LotsViewSet(viewsets.ModelViewSet):
             return MapSerializer(*args, **kwargs)
         return super().get_serializer(*args, **kwargs)
 
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+
     @action(detail=False)
-    def price_odr(self, request, *args, **kwargs):
+    def map(self, request, *args, **kwargs):
+
         serializer = self.get_serializer(self.queryset, many=True)
         return Response(serializer.data)
 
 
-def distance(origin, destination, radius=1):
-    lat1, lon1 = origin
-    lat2, lon2 = destination
 
-    dlat = math.radians(lat2 - lat1)
-    dlon = math.radians(lon2 - lon1)
-    a = math.sin(dlat / 2) * math.sin(dlat / 2) + math.cos(math.radians(lat1)) \
-        * math.cos(math.radians(lat2)) * math.sin(dlon / 2) * math.sin(dlon / 2)
-    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
-    d = radius * c
 
+
+
+
+
+
+
+
+
+
+
+
+    # @action(detail=False)
+    # def price_odr(self, request, *args, **kwargs):
+    #     serializer = self.get_serializer(self.queryset, many=True)
+    #     return Response(serializer.data)
+    # @action(detail=False)
+    # def distance_odr(self, request, *args, **kwargs):
+    #     serializer = self.get_serializer(self.queryset, many=True)
+    #     return Response(serializer.data)
